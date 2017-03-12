35643399df7bfa9ed951fe0c338bae52c2414ec9
Jorge Eduardo Valle <jorge.e.valle@gmail.com>
2017-03-12 17:30:05 -0400

    Set app_server routes for blog and single post views.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
 var ctrlPages = require('../controllers/pages');
+var ctrlQueries = require('../controllers/queries');
 
 /* GET home page */
 router.get('/', ctrlMain.index);
 
-/* GET page by title */
-router.get('/:pageUrl', ctrlPages.pageByTitle);
+/* GET page by url */
+router.get('/blog/:pageUrl', ctrlPages.pageByUrl);
+
+/* GET queries */
+router.get('/blog', ctrlQueries.queryAll);
 
 module.exports = router;
